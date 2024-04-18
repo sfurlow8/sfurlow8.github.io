f38d47f8f0b853e918c071fa0a2f6f3cf0506699
sfurlow8 <sophiefurlow2023@u.northwestern.edu>
2024-04-17 19:08:56 -0700

    update index

--- script.js
+++ script.js
@@ -15,3 +15,72 @@ function showSection(sectionId) {
 document.addEventListener("DOMContentLoaded", function() {
     showSection('aboutme'); // Change 'bio' to the ID of the default section you want to show
 });
+
+// function showDetails(projectId) {
+//     var modal = document.getElementById('modal');
+//     var modalContent = document.getElementById('modal-content');
+//     var modalOverlay = document.getElementById('modal-overlay');
+
+//     // Get project details
+//     var projectDetails = document.getElementById(projectId).innerHTML;
+
+//     // Display modal with project details
+//     modal.classList.add('show');
+//     modalOverlay.style.display = 'block';
+//     modalContent.innerHTML = projectDetails;
+// }
+
+// function hideDetails() {
+//     var modal = document.getElementById('modal');
+//     var modalOverlay = document.getElementById('modal-overlay');
+
+//     modal.classList.remove('show');
+//     modalOverlay.style.display = 'none';
+// }
+
+// function closeOverlay() {
+//     var modal = document.getElementById('modal');
+//     var overlay = document.getElementById('overlay');
+    
+//     modal.style.display = 'none';
+//     overlay.style.display = 'none';
+// }
+
+// // Assuming you have a modal close functionality
+// document.querySelector('.close-button').addEventListener('click', function() {
+//     document.getElementById('projectModal').style.display = "none";
+// });
+
+// Get all project boxes
+var projectBoxes = document.querySelectorAll('.project');
+
+// Add click event listener to each project box
+projectBoxes.forEach(function(projectBox) {
+    projectBox.addEventListener('click', function() {
+        // Get project details from the clicked project box
+        var projectDetails = projectBox.querySelector('.project-content').innerHTML;
+        
+        // Display modal with project details
+        showDetails(projectDetails);
+    });
+});
+
+// Function to display modal with project details
+function showDetails(projectDetails) {
+    var modal = document.getElementById('modal');
+    var overlay = document.getElementById('overlay');
+    var modalContent = document.getElementById('modal-content');
+
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+    modalContent.innerHTML = projectDetails;
+}
+
+// Function to close modal and overlay
+function hideDetails() {
+    var modal = document.getElementById('modal');
+    var overlay = document.getElementById('overlay');
+
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
